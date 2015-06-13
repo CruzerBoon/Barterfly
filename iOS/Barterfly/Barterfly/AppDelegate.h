71c0fa4bf475d82b6c26f08e793396b30c5124a5
MunFai <munfai@live.com>
2015-06-13 10:23:59 +0800

    commit iOS base

---
+++ iOS/Barterfly/Barterfly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Barterfly
+//
+//  Created by Mun Fai Leong on 6/13/15.
+//  Copyright (c) 2015 angelhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Barterfly/Barterfly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Barterfly
+//
+//  Created by Mun Fai Leong on 6/13/15.
+//  Copyright (c) 2015 angelhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
