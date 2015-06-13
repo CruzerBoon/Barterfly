adc6c6876b9cf493f3c90fa941fbe31c569e8b13
CruzerBoon <wai85my@gmail.com>
2015-06-13 11:18:42 +0800

    Re-create Web Api project

---
+++ Dot Net/WebApi_2/angel2015klService/App_Start/WebApiConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+using angel2015klService.DataObjects;
+using angel2015klService.Models;
+
+namespace angel2015klService
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            // Use this class to set configuration options for your mobile service
+            ConfigOptions options = new ConfigOptions();
+
+            // Use this class to set WebAPI configuration options
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            
+            Database.SetInitializer(new angel2015klInitializer());
+        }
+    }
+
+    public class angel2015klInitializer : ClearDatabaseSchemaIfModelChanges<angel2015klContext>
+    {
+        protected override void Seed(angel2015klContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
---
+++ Dot Net/WebApi_2/angel2015klService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="angel2015klService.WebApiApplication" Language="C#" %>
